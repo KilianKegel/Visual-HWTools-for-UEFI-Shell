b0048c3b437e1eb8399602f12e7d1be7619d13e1
KilianKegel <kilian_kegel@hotmail.com>
2023-02-12 19:32:59 +0100

    ### 20230212
    * update to **TORO C Library** *20230212*

--- PCISpy/PCIEView.c
+++ PCISpy/PCIEView.c
@@ -1,7 +1,7 @@
 /*!
 @copyright
 
-    Copyright (c) 2020, Kilian Kegel. All rights reserved.
+    Copyright (c) 2020-2023, Kilian Kegel. All rights reserved.
     This program and the accompanying materials are licensed and made
     available under the terms and conditions of the BSD License
     which accompanies this distribution.  The full text of the license
